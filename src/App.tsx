cb67b9a5659806a67ab8162cb981a3bfc6b6c03b
njc171 <njc171@uclive.ac.nz>
2024-08-30 02:23:23 +1200

    Create about component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import About from './components/about/about'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/about/about.tsx
@@ -0,0 +1,19 @@
+import './about.css';
+
+const About = () => {
+    return (
+        <div>
+           <div className="px-5 max-w-[1560px] mx-auto min-h-screen flex items-center justify-center">
+                <div>
+                    <h1 className="font-semibold text-[32px] mb-3 text-center">
+                        Hello, I am a <span className="text-[#007BFF]">computer science</span> student and developer
+                    </h1>
+                    <p className="my-6 text-center">My aim is to learn and develop my skills as a programmer, designer, and developer</p>
+                    <button className="font-medium py-2 px-4 border duration-200 border-[#007BFF] hover:bg-[#007BFF33]">Contact me!</button>
+                </div>
+            </div> 
+        </div>
+    );
+}
+
+export default About;
